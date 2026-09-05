b290a2dd0b97647be056c76115d89ecc6c2e07da
Yuu <122996864+Id-Yuu@users.noreply.github.com>
2026-09-05 07:10:00 +0700

    Update dictionary.js

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,58 +1,44 @@
-const DictionaryModule = (function () {
-    const officialEntries = [
-        ['gibran', ['kosong', 'bengong']],
-        ['tedy', ['sok sibuk']],
-        ['prabowo', ['asbun', 'gemoy']],
-        ['jokowi', ['bohong']],
-        ['kdm', ['sibuk ngonten']],
-        ['anies', ['nganggur', 'baca']],
-        ['ganjar', ['lari', 'nonton bkp']],
-        ['bj habibie', ['pintar']],
-        ['nanik', ['nangis']],
-        ['cak imin', ['saya gak tahu']],
-        ["ma'ruf amin", ['gak ngapa-ngapain']],
-        ['purbaya', ['pusing']],
-        ['raffi', ['nyuci']],
-        ['uah', ['damai']],
-        ['meutya', ['blokir', 'take down']],
-        ['bahlil', ['gosong']],
-        ['isam', ['serakah']],
-        ['febrie', ['emas']],
-        ['megawati', ['sok cantik']],
-        ['ridwan kamil', ['selingkuh']],
-        ['hercules', ['anarkis']],
-        ['ahok', ['marah']],
-        ['sby', ['santai', 'nyantai']],
-        ['yusuf mansur', ['duitnya mana', 'gak ada duit']]
-    ];
+const DICTIONARY = {
+  "Eksekutif & Menteri Negara": [
+    { target: "Prabowo",     sinonim: ["asbun", "gemoy"] },
+    { target: "Gibran",      sinonim: ["kosong", "bengong"] },
+    { target: "Jokowi",      sinonim: ["bohong"] },
+    { target: "Ma'ruf Amin", sinonim: ["gak ngapa-ngapain"] },
+    { target: "Meutya",      sinonim: ["blokir", "take down"] },
+    { target: "Bahlil",      sinonim: ["gosong"] },
+    { target: "Purbaya",     sinonim: ["pusing"] },
+  ],
+  "Kepala Daerah": [
+    { target: "KDM", sinonim: ["sibuk ngonten"] },
+  ],
+  "Politisi & Tokoh Partai": [
+    { target: "Cak Imin",     sinonim: ["saya gak tahu"] },
+    { target: "Megawati",     sinonim: ["sok cantik"] },
+    { target: "Ridwan Kamil", sinonim: ["selingkuh"] },
+    { target: "Ganjar",       sinonim: ["lari", "nonton bkp"] },
+    { target: "Anies",        sinonim: ["nganggur", "baca"] },
+    { target: "Ahok",         sinonim: ["marah"] },
+    { target: "SBY",          sinonim: ["santai", "nyantai"] },
+  ],
+  "Aparat & Institusi Hukum": [
+    { target: "Febrie", sinonim: ["emas"] },
+  ],
+  "Pengusaha": [
+    { target: "Isam", sinonim: ["serakah"] },
+  ],
+  "Tokoh Agama": [
+    { target: "UAH",          sinonim: ["damai"] },
+    { target: "Yusuf Mansur", sinonim: ["duitnya mana", "gak ada duit"] },
+  ],
+  "Selebriti & Figur Publik": [
+    { target: "Raffi",    sinonim: ["nyuci"] },
+    { target: "Nanik",    sinonim: ["nangis"] },
+    { target: "Hercules", sinonim: ["anarkis"] },
+    { target: "Tedy",     sinonim: ["sok sibuk"] },
+  ],
+  "Tokoh Sejarah / Ilmuwan": [
+    { target: "BJ Habibie", sinonim: ["pintar"] },
+  ]
+};
 
-    const slangToIndonesian = new Map(
-        officialEntries.map(([official, meanings]) => [official, meanings.join(', ')])
-    );
-    const indonesianToSlang = new Map();
-
-    officialEntries.forEach(([official, meanings]) => {
-        meanings.forEach((meaning) => indonesianToSlang.set(meaning, official));
-    });
-
-    function getMap(reverse = false) {
-        return reverse ? indonesianToSlang : slangToIndonesian;
-    }
-
-    function getSortedKeys(reverse = false) {
-        return Array.from(getMap(reverse).keys()).sort((a, b) => b.length - a.length);
-    }
-
-    function getAccordionGroups(reverse = false) {
-        const dictionary = getMap(reverse);
-        return [{
-            title: reverse ? 'Bahasa Indonesia' : 'Nama Pejabat',
-            description: reverse
-                ? 'Klik kata Bahasa Indonesia untuk menambahkannya ke teks.'
-                : 'Klik nama pejabat untuk menambahkannya ke teks.',
-            entries: Array.from(dictionary, ([key, value]) => ({ key, value }))
-        }];
-    }
-
-    return { getMap, getSortedKeys, getAccordionGroups };
-})();
+export default DICTIONARY;
